6124ab4b47e7b4b5e0ecadff99d22ba911fef27e
Flordeliza Mae Canlas <maemaeperlas@gmail.com>
2022-08-30 18:55:52 -0700

    DONE

--- app/controllers/toys_controller.rb
+++ app/controllers/toys_controller.rb
@@ -1,19 +1,22 @@
 class ToysController < ApplicationController
   wrap_parameters format: []
 
+rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
+
   def index
     toys = Toy.all
     render json: toys
   end
 
   def create
-    toy = Toys.create(toy_params)
+    toy = Toy.create!(toy_params)
     render json: toy, status: :created
   end
 
   def update
     toy = Toy.find_by(id: params[:id])
     toy.update(toy_params)
+    render json: toy
   end
 
   def destroy
@@ -26,6 +29,11 @@ class ToysController < ApplicationController
   
   def toy_params
     params.permit(:name, :image, :likes)
+  end
+
+  def render_unprocessable_entity_response (invalid)
+    render json: { errors: invalid.record.errors.full_messages},
+    status: :unprocessable_entity 
   end
 
 end
